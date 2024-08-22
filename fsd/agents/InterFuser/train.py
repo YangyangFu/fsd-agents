c3cdace5b9ab4b4448dfcbea0bffa40243971b18
Yangyang Fu <fuyy2008@gmail.com>
2024-08-22 10:06:18 -0500

    added training code for inteffuser

---
+++ fsd/agents/InterFuser/train.py
@@ -0,0 +1,9 @@
+from mmengine import Config
+from mmengine.registry import init_default_scope
+from fsd.registry import RUNNERS 
+
+cfg = Config.fromfile('fsd/agents/InterFuser/configs/interfuser_r50_carla.py')
+init_default_scope('fsd')
+runner = RUNNERS.build(cfg)
+
+runner.train()
