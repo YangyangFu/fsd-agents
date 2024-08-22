993f8d80cb500411acec86ebaa968b68d6bff9b0
Yangyang Fu <fuyy2008@gmail.com>
2024-08-22 10:05:53 -0500

    added agent-specific transform

---
+++ fsd/agents/InterFuser/interfuser/transform.py
@@ -0,0 +1,37 @@
+from typing import Mapping, Optional, Sequence, Union, Tuple, List
+from mmcv.image import imresize
+
+from fsd.registry import AGENT_TRANSFORMS
+from fsd.datasets.transforms import center_crop
+from .utils import generate_density_map
+
+@AGENT_TRANSFORMS.register_module()
+class InterFuserDensityMap(object):
+    """Generate object density map data for interfuser model
+
+    """
+    def __init__(self, 
+                 bev_range: Sequence[float], 
+                 pixels_per_meter: Optional[int] = 8):
+        """Initialize DensityMap
+        Args:
+            bev_range (Sequence[float]): [xmin, xmax, ymin, ymax]
+            pixels_per_meter (Optional[int], optional): Defaults to 8.
+        """
+        super().__init__()
+        self.bev_range = bev_range
+        self.pixels_per_meter = pixels_per_meter
+        
+    def __call__(self, results):
+        """Preprocess data for interfuser model
+        Args:
+            results (Mapping[str, Any]): Input data.
+
+        """
+        gt_bboxes = results['anno_info']['gt_bboxes_3d']
+        density_map = generate_density_map(gt_bboxes, self.bev_range, self.pixels_per_meter)
+        results['anno_info']['gt_grid_density'] = density_map
+        return results
+
+        
+        
