b28c0eeb0ec6c51f4c9bd03ed197aca6d1391946
Yangyang Fu <fuyy2008@gmail.com>
2024-08-23 11:11:52 -0500

    remove unneeded data strucutures

--- fsd/structures/__init__.py
+++ fsd/structures/__init__.py
@@ -1,11 +1,2 @@
-# Copyright (c) Facebook, Inc. and its affiliates.
-from .boxes import BoxMode, Boxes
-from .instances import Instances
-# from .keypoints import Keypoints, heatmaps_to_keypoints
-from .masks import ROIMasks
-# from .masks import BitMasks, PolygonMasks, polygons_to_bitmask, ROIMasks
-# from .rotated_boxes import RotatedBoxes
-# from .rotated_boxes import pairwise_iou as pairwise_iou_rotated
-
 from .fsd_data import TrajectoryData
 from .fsd_data_sample import PlanningDataSample
